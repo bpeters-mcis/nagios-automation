2d27eed26dd8b6efd0e951828c8393d5101f9c6e
Ben Peters <bpeters@emich.edu>
2016-01-28 16:20:23 -0500

    Added automatic downtime for tier 4 servers

--- update_servers.sh
+++ update_servers.sh
@@ -8,3 +8,21 @@ touch /usr/local/nagios/etc/Servers_last_imported.txt
 
 # Restart nagios service to load the new configs
 service nagios restart
+
+# Get the day of the week
+day=$(date +%a)
+
+# If it's Wednesday, mark reboot servers for downtime
+if [ $day == 'Wed' ]; then
+
+    # Set a downtime start to be ~10pm tonight
+    datetime=`date +%s`
+    starttime=`date +%s -d "+22 hours"`
+    endtime=`date +%s -d "+24 hours"`
+    echo "yay"
+    # Send the downtime request
+    echo "[$datetime] SCHEDULE_HOSTGROUP_HOST_DOWNTIME;Auto-Patch-And-Reboot;$starttime;$endtime;1;0;7200;bpeters-AD;Test Downtime" > /usr/local/nagios/var/rw/nagios.cmd
+
+fi
+
+
