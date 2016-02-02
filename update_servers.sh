2b0044074309e931b9f63da5d2338997057881e3
Ben Peters <bpeters@emich.edu>
2016-02-02 15:15:15 -0500

    Fixed shell script

--- update_servers.sh
+++ update_servers.sh
@@ -10,30 +10,31 @@ RESTART="No"
 
 # Now, check to see if any of the config files are actually different.
 if cmp -s objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg ; then
+    rm objects/servers_from_lansweeper_new.cfg
+else
     rm objects/servers_from_lansweeper.cfg
     mv objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg
     RESTART="Yes"
     echo "Server definitions are different."
-else
-    rm objects/servers_from_lansweeper_new.cfg
 fi
 
 if cmp -s objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg ; then
+    rm objects/contacts_from_ad_new.cfg
+else
+
     rm objects/contacts_from_ad.cfg
     mv objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg
     RESTART="Yes"
     echo "Contact definitions are different."
-else
-    rm objects/contacts_from_ad_new.cfg
 fi
 
 if cmp -s cgi_new.cfg cgi.cfg ; then
+    rm cgi_new.cfg
+else
     rm cgi.cfg
     mv cgi_new.cfg cgi.cfg
     RESTART="Yes"
     echo "CGI definitions are different."
-else
-    rm cgi_new.cfg
 fi
 
 if [ $RESTART == "Yes" ] ; then
