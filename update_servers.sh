34ac6a53edd5260d15c5116510692fc1553aa656
Ben Peters <bpeters@emich.edu>
2016-02-02 08:38:55 -0500

    Changed shell scripts

--- update_servers.sh
+++ update_servers.sh
@@ -6,14 +6,31 @@ php /usr/local/nagios/etc/group_contact_generation.php
 # Note when this was last run
 touch /usr/local/nagios/etc/Servers_last_imported.txt
 
+RESTART="No"
+
 # Now, check to see if any of the config files are actually different.
-if [ diff objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg >/dev/null ] || [ diff objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg >/dev/null ] || [ diff cgi_new.cfg cgi.cfg >/dev/null ] ; then
+if [ diff objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg >/dev/null ] ; then
+    mv objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg
+    RESTART="Yes"
+else
+    rm objects/servers_from_lansweeper_new.cfg
+fi
 
-  # Replace the config files with the new ones, since they are different
-  mv objects/servers_from_lansweeper_new.cfg objects/servers_from_lansweeper.cfg
-  mv objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg
-  mv cgi_new.cfg cgi.cfg
+if [ diff objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg >/dev/null ] ; then
+    mv objects/contacts_from_ad_new.cfg objects/contacts_from_ad.cfg
+    RESTART="Yes"
+else
+    rm objects/contacts_from_ad_new.cfg
+fi
 
+if [ diff cgi_new.cfg cgi.cfg >/dev/null ] ; then
+    mv cgi_new.cfg cgi.cfg
+    RESTART="Yes"
+else
+    rm cgi_new.cfg
+fi
+
+if [ $RESTART == "Yes" ] ; then
   # Restart nagios service to load the new configs
   service nagios restart
 fi
