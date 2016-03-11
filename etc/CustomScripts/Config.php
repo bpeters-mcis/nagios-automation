e5e4793e14527650fa81ca11c4553dd1e44571a4
Ben Peters <bpeters@emich.edu>
2016-03-11 09:46:00 -0500

    Removed one server from ignore list

--- etc/CustomScripts/Config.php
+++ etc/CustomScripts/Config.php
@@ -105,7 +105,7 @@ class Config {
 
     # These servers will be completely ignored, and will never be included in monitoring.  This is useful if there's a system with someone else's nagios or something,
     # that we don't want conflicting with ours I guess.
-    public static $ServersToIgnore = array('INTLTESTDB', 'INTLDB');
+    public static $ServersToIgnore = array('INTLDB');
 
     # Comma separated list of any contact group(s) that should be added to ALL servers
     public static $ContactGroupForAllServers = 'WindowsTeam';
