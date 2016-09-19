98407e0f95aabf096e69e0b8d584643bcb1f916a
Ben Peters <bpeters@emich.edu>
2016-09-19 08:54:31 -0400

    Added IDM2 people.campus monitor downtime

--- etc/schedule_downtime.sh
+++ etc/schedule_downtime.sh
@@ -5,19 +5,24 @@
 ###################################################################
 
 # Downtime Date Arrays
-TESTDATES=(2016-05-13 2016-06-03 2016-07-15 2016-07-29 2016-09-16 2016-10-07)
-PRODDATES=(2016-05-20 2016-06-10 2016-07-22 2016-08-05 2016-09-23 2016-10-14)
+TESTDATES=(2016-10-14 2016-11-04 2016-11-18)
+PRODDATES=(2016-09-23 2016-10-21 2016-11-11 2016-12-02)
 
 # Get the day of the week, and the full date
 day=$(date +%a)
 DATE=`date +%Y-%m-%d`
+datetime=`date +%s`
+
+# Schedule downtime for the people.campus replication - we know that happens every night, don't sweat it
+starttime=`date +%s -d "+1 hours"`
+endtime=`date +%s -d "+4 hours"`
+echo "[$datetime] SCHEDULE_SVC_DOWNTIME;idm2.emich.edu;People.campus Replication;$starttime;$endtime;1;0;7200;bpeters-AD;People.campus Downtime" > /usr/local/nagios/var/rw/nagios.cmd
 
 
 # If it's Wednesday, mark reboot servers for downtime
 if [ $day == 'Wed' ]; then
 
     # Set a downtime start to be ~10pm tonight, end at midnight
-    datetime=`date +%s`
     starttime=`date +%s -d "+22 hours"`
     endtime=`date +%s -d "+24 hours"`
     # Send the downtime request
@@ -29,7 +34,6 @@ fi
 if [[ " ${TESTDATES[*]} " == *" $DATE "* ]]; then
 
     # Set a downtime start to be ~12pm today, end at 5pm
-    datetime=`date +%s`
     starttime=`date +%s -d "+675 minutes"`
     endtime=`date +%s -d "+975 minutes"`
     # Send the downtime request
@@ -42,7 +46,6 @@ fi
 if [[ " ${PRODATES[*]} " == *" $DATE "* ]]; then
 
     # Set a downtime start to be ~5:45pm today, end at 11pm
-    datetime=`date +%s`
     starttime=`date +%s -d "+1035 minutes"`
     endtime=`date +%s -d "+1350 minutes"`
     # Send the downtime request
